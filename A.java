f2e7bf38f143c26a96145b442173d55ff2def10f
Cho Seongeun <seongeun.dev@gmail.com>
2022-07-07 14:54:20 +0900

    add ;;

--- A.java
+++ A.java
@@ -3,7 +3,7 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("Hi! I am YoonSun");
-		System.out.println("Welcome! I'm Jisu :D")
-		System.out.println("I'm SE")
+		System.out.println("Welcome! I'm Jisu :D");
+		System.out.println("I'm SE");
 	}
 }
